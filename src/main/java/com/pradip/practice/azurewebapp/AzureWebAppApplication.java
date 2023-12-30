661e99c22b606087971942b4cf3fcef5fbb0a941
Pradip Sarbabhouma <contact.pradipks@gmail.com>
2023-12-30 02:19:28 +0000

    initial source code

---
+++ src/main/java/com/pradip/practice/azurewebapp/AzureWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.pradip.practice.azurewebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureWebAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pradip/practice/azurewebapp/AzureWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pradip.practice.azurewebapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AzureWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
